3b050fd3eebebc0de007ebdbbd31fa10ad9fc851
gustavo.nogueira <e13839@sebraemg.com.br>
2026-01-28 13:01:48 -0300

    form page header

--- src/app/novo-livro/page.tsx
+++ src/app/novo-livro/page.tsx
@@ -1,21 +1,11 @@
-import { Button } from "@/src/components/ui/button";
-import { ArrowLeft, Save } from "lucide-react";
+import { NewBookHeader } from "@/src/features/NewBook/components/NewBookHeader";
 
 const NewBookPage = () => {
   return (
     <main className="flex flex-col">
-      <header className="flex justify-between">
-        <div className="flex space-x-6 items-center">
-          <ArrowLeft />
-          <h1 className="text-lg sm:text-2xl font-semibold">
-            Adicionar Novo Livro
-          </h1>
-        </div>
-        <Button>
-          <Save />
-          <p>Salvar</p>
-        </Button>
-      </header>
+      <NewBookHeader />
+
+      <form></form>
     </main>
   );
 };
---
+++ src/features/NewBook/components/NewBookHeader.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Button } from "@/src/components/ui/button";
+import { ArrowLeft, Save } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export const NewBookHeader = () => {
+  const router = useRouter();
+
+  return (
+    <header className="flex justify-between">
+      <div className="flex space-x-6 items-center">
+        <Button onClick={() => router.back()} variant="ghost">
+          <ArrowLeft />
+        </Button>
+        <h1 className="text-lg sm:text-2xl font-semibold">
+          Adicionar Novo Livro
+        </h1>
+      </div>
+      <Button>
+        <Save />
+        <p>Salvar</p>
+      </Button>
+    </header>
+  );
+};
