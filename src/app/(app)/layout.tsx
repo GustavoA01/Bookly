32589aa546f033aee16b661545a5cc91b726e5f6
GustavoA01 <gustavoaguiar14325@gmail.com>
2026-05-05 17:07:58 -0300

    correcao responsividade tela xl

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,7 +3,7 @@ import { Header } from '@/src/components/Header';
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => (
   <div>
-    <main className="flex flex-col space-y-2 min-h-screen container mx-auto py-8 max-sm:px-4">
+    <main className="flex flex-col space-y-2 min-h-screen container mx-auto py-8 xl:px-8 max-sm:px-4">
       <Header />
       {children}
     </main>
